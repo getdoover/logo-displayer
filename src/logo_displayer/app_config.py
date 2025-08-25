032df7843519dc554167fe181ed66c0315da1873
Jarrod <jarrod@span-eng.com>
2025-08-25 10:44:59 +1000

    working logo displayer

--- src/logo_displayer/app_config.py
+++ src/logo_displayer/app_config.py
@@ -5,11 +5,17 @@ from pydoover import config
 
 class LogoDisplayerConfig(config.Schema):
     def __init__(self):
-        self.outputs_enabled = config.Boolean("Digital Outputs Enabled", default=True)
-        self.funny_message = config.String("A Funny Message")  # this will be required as no default given.
-
-        self.sim_app_key = config.Application("Simulator App Key", description="The app key for the simulator")
-
+        self.logo_url = config.String(
+            "Logo URL", 
+            description="The URL of the logo to display",
+            default="https://doover.com/wp-content/uploads/2025/02/Blue-Background-2.png"
+        )
+        
+        self.max_logo_height = config.Number(
+            "Max Logo Height", 
+            description="The maximum height of the logo in pixels",
+            default=150.0
+        )
 
 def export():
     LogoDisplayerConfig().export(Path(__file__).parents[2] / "doover_config.json", "logo_displayer")
--- src/logo_displayer/application.py
+++ src/logo_displayer/application.py
@@ -1,4 +1,5 @@
 import logging
+import asyncio
 import time
 
 from pydoover.docker import Application
@@ -6,50 +7,20 @@ from pydoover import ui
 
 from .app_config import LogoDisplayerConfig
 from .app_ui import LogoDisplayerUI
-from .app_state import LogoDisplayerState
 
 log = logging.getLogger()
 
 class LogoDisplayerApplication(Application):
     config: LogoDisplayerConfig  # not necessary, but helps your IDE provide autocomplete!
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
         self.started: float = time.time()
         self.ui: LogoDisplayerUI = None
-        self.state: LogoDisplayerState = None
-
+    
     async def setup(self):
-        self.ui = LogoDisplayerUI()
-        self.state = LogoDisplayerState()
+        self.ui = LogoDisplayerUI(self.config)
         self.ui_manager.add_children(*self.ui.fetch())
+        self.ui_manager.set_variant(ui.ApplicationVariant.stacked)
 
     async def main_loop(self):
-        log.info(f"State is: {self.state.state}")
-
-        # a random value we set inside our simulator. Go check it out in simulators/sample!
-        random_value = self.get_tag("random_value", self.config.sim_app_key.value)
-        log.info("Random value from simulator: %s", random_value)
-
-        self.ui.update(
-            True,
-            random_value,
-            time.time() - self.started,
-        )
-
-    @ui.callback("send_alert")
-    async def on_send_alert(self, new_value):
-        log.info(f"Sending alert: {self.ui.test_output.current_value}")
-        await self.publish_to_channel("significantAlerts", self.ui.test_output.current_value)
-        self.ui.send_alert.coerce(None)
-
-    @ui.callback("test_message")
-    async def on_text_parameter_change(self, new_value):
-        log.info(f"New value for test message: {new_value}")
-        # Set the value as an output to the corresponding variable is this case
-        self.ui.test_output.update(new_value)
-
-    @ui.callback("charge_mode")
-    async def on_state_command(self, new_value):
-        log.info(f"New value for state command: {new_value}")
+        pass
